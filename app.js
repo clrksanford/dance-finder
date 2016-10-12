4ee77a228dc181dc48ac636511ddefc9bab60a38
Clark Sanford <clrksanford@gmail.com>
2016-10-12 13:00:55 -0500

    Clean and parse html from site

--- app.js
+++ app.js
@@ -3,5 +3,19 @@ const axios = require('axios');
 
 axios.get('http://www.contradancelinks.com/schedule_AL.html')
   .then(function(response) {
-    console.log(response);
+    var goodString = response.data.replace("<BR><BR>\n<DD>", "<BR><BR></DD>\n<DD>");
+    let $ = cheerio.load(goodString, {
+      ignoreWhitespace: true,
+      lowerCaseTags: true
+    });
+
+    $('dd').each(function(i,dd) {
+      var text = $(this).text();
+      var [startDate, rest] = text.split(' Dance Series: ');
+      startDate = new Date(startDate);
+
+      var [danceSeries, rest] = rest.split(' Location: ');
+      var [location, rest] = rest.split(' Caller(s): ');
+      var [callers, rest] = rest.split(' Band(s)/Musician(s): ');
+      var [bands, rest] = rest.split(' Note: ');
   });
