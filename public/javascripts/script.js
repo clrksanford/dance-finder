bbb0dceacf8c4f968205e21ee733878e465c3ea5
Clark Sanford <clrksanford@gmail.com>
2016-10-16 16:11:12 -0500

    add headings for search results, output only next 10 dances

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -16,18 +16,23 @@ function ajaxCall(query) {
   })
     .done(function (response) {
 
-      $('#results').empty();
-      
-      response.forEach(function (item) {
+      // If h2's are invisible, show them
+      $('h2').css('visibility', 'visible');
+      $('.state').text(response[0].state);
+      $('ul').empty();
+
+
+      for(var i=0; i < 10; i++) {
+        var item = response[i];
         var series = item.series;
         var caller = item.callers;
         var date = new Date(item.date).toString().split(' 00:')[0];
         var location = item.location;
         var state = item.state;
 
-        var resultsList = $('#results');
+        var resultsList = $('#upcoming');
         var newLi = $('<li></li>');
-        var h3 = $('<h3 />').text(series);
+        var h3 = $('<h6 />').text(series);
         var p1 = $('<p />').text(date);
         var p2 = $('<p />').text(caller);
         var p3 = $('<p />').text(location);
@@ -35,6 +40,6 @@ function ajaxCall(query) {
 
         newLi.append(h3, p1, p2, p3, p4);
         resultsList.append(newLi);
-      });
+      }
     });
 }
