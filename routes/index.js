e937b2cc489c05ccd8641b90e1680f3e4eebbb3b
Clark Sanford <clrksanford@gmail.com>
2016-10-15 19:48:57 -0500

    Make sure router serves DB info

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var _ = require('lodash');
+var Upcoming = require('../models/upcoming');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  Upcoming.findById('58029f046b6e6f1933e8da7c', function (err, dance) {
+    if (err) {
+      console.log('error');
+      res.status(500).send();
+    } else if (!dance) {
+      console.log('no song found');
+      res.status(404).send();
+    } else {
+      console.log(dance);
+      res.json(dance);
+    }
+  });
 });
 
 module.exports = router;
