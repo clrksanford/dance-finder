69f8172c73b818e38b0e78d06e3e029a04195931
Clark Sanford <clrksanford@gmail.com>
2016-10-16 11:24:40 -0500

    Format date

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -15,23 +15,28 @@ function ajaxCall(query) {
     url: "/" + query
   })
     .done(function (response) {
-      response.forEach(function (item) {
-        var series = item.series;
-        var caller = item.callers;
-        var date = item.date;
-        var location = item.location;
-        var state = item.state;
+      var date = new Date(response.date).toString().split(' 00:')[0];
 
-        var resultsList = $('#results');
-        var newLi = $('<li></li>');
-        var h3 = $('<h3 />').text(series);
-        var p1 = $('<p />').text(date);
-        var p2 = $('<p />').text(caller);
-        var p3 = $('<p />').text(location);
-        var p4 = $('<p />').text(state);
-
-        newLi.append(h3, p1, p2, p3, p4);
-        resultsList.append(newLi);
-      });
+      console.log(date);
     });
 }
+
+
+// response.forEach(function (item) {
+//   var series = item.series;
+//   var caller = item.callers;
+//   var date = item.date;
+//   var location = item.location;
+//   var state = item.state;
+//
+//   var resultsList = $('#results');
+//   var newLi = $('<li></li>');
+//   var h3 = $('<h3 />').text(series);
+//   var p1 = $('<p />').text(date);
+//   var p2 = $('<p />').text(caller);
+//   var p3 = $('<p />').text(location);
+//   var p4 = $('<p />').text(state);
+//
+//   newLi.append(h3, p1, p2, p3, p4);
+//   resultsList.append(newLi);
+// });
